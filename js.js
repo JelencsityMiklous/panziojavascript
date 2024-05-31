5f34ec7e5dc2663fa3de7e169c14ac423b105b36
JelencsityMiklous <jelencsitymiklos@turr.hu>
2024-05-31 09:40:42 +0200

    Javascript fejlesztése, vendégek száma és összköltség befejezése

--- js.js
+++ js.js
@@ -5,10 +5,14 @@ function Szamolas(){
     let erkezes = new Date (document.getElementById("erk").value)
     let tavozas = new Date (document.getElementById("tav").value)
 
-    let napokszama = tavozas-erkezes
+    let napokszama = (tavozas-erkezes)/(1000*3600*24)
     
-    vendegekszama = document.getElementById("vendegekszama")
+    vendegekszama = Number(document.getElementById("vendegekszama").value)
 
+    let eletkor1 = document.getElementById("eletkor1")
+    let eletkor2 = document.getElementById("eletkor2")
+    let eletkor3 = document.getElementById("eletkor3")
+    let eletkor4 = document.getElementById("eletkor4")
 
     
     let szobavalasztas = document.querySelector('input[name="radio"]:checked')
@@ -82,14 +86,25 @@ function Szamolas(){
         szolgaltatasok="Nincsen egyéb szolgáltatás"
     }
     
-    
-
-
+    osszkoltseg=osszkoltseg*napokszama*vendegekszama
 
     if(erkezes<tavozas){
 
-        if(vendegekszama.value>0){
-        alert(`Kedves Vendégünk!\n\nTájékoztatjuk a sikeres foglalásáról\n\nÉrkezés: ${erkezes.toLocaleDateString()}\nTavozás: ${tavozas.toLocaleDateString()}\nSzoba típusa: ${szobatipus}\nVendégek száma: ${vendegekszama.value}\nEllátás: ${ellatastipus}\nIgényelt szolgáltatások: ${szolgaltatasok}`);
+        if(vendegekszama>0 && vendegekszama<5){
+
+            if(szobavalasztas.value==1 && vendegekszama==1 || szobavalasztas.value==2 && vendegekszama==2 || szobavalasztas.value==3 && vendegekszama==3 || szobavalasztas.value==4 && vendegekszama==4){
+
+                if((szobavalasztas.value==3 && eletkor1.value<17 || eletkor2.value<17 || eletkor3.value<17 ) || (szobavalasztas.value==4 && eletkor1.value<17 || eletkor2.value<17 || eletkor3.value<17 || eletkor4.value17)){
+                    alert(`Kedves Vendégünk!\n\nTájékoztatjuk a sikeres foglalásáról\n\nÉrkezés: ${erkezes.toLocaleDateString()}\nTavozás: ${tavozas.toLocaleDateString()}\nSzoba típusa: ${szobatipus}\nVendégek száma: ${vendegekszama}\nEllátás: ${ellatastipus}\nIgényelt szolgáltatások: ${szolgaltatasok}\nA teljes összeg: ${osszkoltseg}\n\nKöszönjük megrendelését!`);
+                }
+                else{
+                    alert("Az életkor határ a pótágynál 16 év")
+                }
+                
+            }
+            else{
+                alert("Hibás a szobaválasztás/vendégek száma")
+            }
         }
         else{
             alert("Vendégek száma nem megfelelő")
@@ -98,7 +113,8 @@ function Szamolas(){
     else{
         alert("Helytelen az érkezés/távozás")
     }
-    alert(`${osszkoltseg}`)
+    
+   
 
 
     
