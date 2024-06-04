70d9cfad480ba734fff65ff1a49eaba4705b44b7
JelencsityMiklous <jelencsitymiklos@turr.hu>
2024-06-04 12:17:57 +0200

    Javascript befejezése, hibák kezelése, reszponzivitás véglegesítése

--- js.js
+++ js.js
@@ -9,10 +9,23 @@ function Szamolas(){
     
     vendegekszama = Number(document.getElementById("vendegekszama").value)
 
-    let eletkor1 = document.getElementById("eletkor1")
-    let eletkor2 = document.getElementById("eletkor2")
-    let eletkor3 = document.getElementById("eletkor3")
-    let eletkor4 = document.getElementById("eletkor4")
+    let eletkorok = [Number(document.getElementById("eletkor1").value),Number(document.getElementById("eletkor2").value),Number(document.getElementById("eletkor3").value),Number(document.getElementById("eletkor4").value)]
+
+
+    let eletkorokhelyessege=true;
+    let kiskoru=0;
+    let nagykoru=0;
+    for(let eletkor of eletkorok){
+        if(Number.isNaN(eletkor)){
+            eletkorokhelyessege=false;
+        }
+        if(eletkor!="" && eletkor<16){
+            kiskoru++;
+        }
+        if(eletkor!="" && eletkor>16){
+            nagykoru++;
+        }
+    }
 
     
     let szobavalasztas = document.querySelector('input[name="radio"]:checked')
@@ -95,7 +108,19 @@ function Szamolas(){
             if(szobavalasztas.value==1 && vendegekszama==1 || szobavalasztas.value==2 && vendegekszama==2 || szobavalasztas.value==3 && vendegekszama==3 || szobavalasztas.value==4 && vendegekszama==4){
 
                 if((szobavalasztas.value==3 && eletkor1.value<17 || eletkor2.value<17 || eletkor3.value<17 ) || (szobavalasztas.value==4 && eletkor1.value<17 || eletkor2.value<17 || eletkor3.value<17 || eletkor4.value17)){
-                    alert(`Kedves Vendégünk!\n\nTájékoztatjuk a sikeres foglalásáról\n\nÉrkezés: ${erkezes.toLocaleDateString()}\nTavozás: ${tavozas.toLocaleDateString()}\nSzoba típusa: ${szobatipus}\nVendégek száma: ${vendegekszama}\nEllátás: ${ellatastipus}\nIgényelt szolgáltatások: ${szolgaltatasok}\nA teljes összeg: ${osszkoltseg}\n\nKöszönjük megrendelését!`);
+                    if(eletkorokhelyessege){
+                        if(vendegekszama==kiskoru+nagykoru){
+                            alert(`Kedves Vendégünk!\n\nTájékoztatjuk a sikeres foglalásáról\n\nÉrkezés: ${erkezes.toLocaleDateString()}\nTavozás: ${tavozas.toLocaleDateString()}\nSzoba típusa: ${szobatipus}\nVendégek száma: ${vendegekszama}\nEllátás: ${ellatastipus}\nIgényelt szolgáltatások: ${szolgaltatasok}\nA teljes összeg: ${osszkoltseg} Ft.\n\nKöszönjük megrendelését!`);
+                        }
+                        else{
+                            alert("A beírt életkorok nem egyeznek a vendégek számával")
+                        }
+                        
+                    }
+                    else{
+                        alert("A beírt életkor nem szám")
+                    }
+                    
                 }
                 else{
                     alert("Az életkor határ a pótágynál 16 év")
